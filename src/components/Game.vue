7b1f95879d8d25f9a3ee46eb2fcee4b8b5a44295
Yuta4u <yudhistira4u@gmail.com>
2022-04-23 16:12:05 +0700

    230422

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -9,55 +9,63 @@ const play = ref(false)
 <template>
   <div v-if="!play">
     <w-flex id="container-dashboard">
-    <w-card id="card" shadow color="white">
-      PLAYER1
-      <div id="bg-img-player1">
-        <img
-          id="img-player"
-          src="https://i.pinimg.com/originals/fd/aa/7a/fdaa7a4fcc3db7e975ad935786e3e57a.png"
-        />
-      </div>
-      <w-button id="bt-ready"  class="ma1" bg-color="success" xl @click="readyp1 = !readyp1">
-        <h1 v-if="!readyp1">Ready</h1>  
-        <h1 v-else>Cancel</h1>
-      </w-button>
-    </w-card>
+      <w-card id="card" shadow color="white">
+        PLAYER1
+        <div id="bg-img-player1">
+          <img
+            id="img-player"
+            src="https://i.pinimg.com/originals/fd/aa/7a/fdaa7a4fcc3db7e975ad935786e3e57a.png"
+          />
+        </div>
+        <div id="color-p">CEK</div>
+        <w-button id="btn-ready-p1" color="primary" text lg
+          ><h2>READY</h2></w-button
+        >
+      </w-card>
 
-    <h1> VS </h1>
-
-    <w-card id="card" shadow>
-      PLAYER2
-      <div id="bg-img-player2">
-        <img
-          id="img-player"
-          src="https://i.pinimg.com/originals/fd/aa/7a/fdaa7a4fcc3db7e975ad935786e3e57a.png"
-        />
-      </div>
-      <w-button id="bt-ready"  class="ma1" bg-color="success" xl @click="readyp2 = !readyp2">
-        <h1 v-if="!readyp2">Ready</h1>  
-        <h1 v-else>Cancel</h1>
-      </w-button>
-    </w-card>
-  </w-flex>
-
-  <w-button
-  @click="play = !play"
-  id="bt-play"
-  class="ma1"
-  bg-color="success"
-  color="yellow-light2">
-  Play
-</w-button>
+      <w-card id="card" shadow>
+        PLAYER2
+        <div id="bg-img-player2">
+          <img
+            id="img-player"
+            src="https://i.pinimg.com/originals/fd/aa/7a/fdaa7a4fcc3db7e975ad935786e3e57a.png"
+          />
+        </div>
+        <div id="color-p">CEK</div>
+        <w-button id="btn-ready-p1" color="primary" text lg
+          ><h2>READY</h2></w-button
+        >
+      </w-card>
+    </w-flex>
   </div>
-  
 </template>
 
 <style>
-#bt-play {
+#color-p {
+  background-color: red;
+}
+
+#btn-ready-p1 {
+  width: 100%;
+  height: 40px;
+  position: absolute;
+  bottom: 50px;
+  left: 0;
+}
+
+#btn-ready-p2 {
+  width: 100%;
+  height: 40px;
+  position: absolute;
+  bottom: 50px;
+  left: 0;
+}
+/* #bt-play {
   width: 300px;
   height: 120px;
   font-size: 70px;
-}
+  bottom: 350px;
+} */
 
 #bt-ready {
   width: 100%;
@@ -72,6 +80,7 @@ const play = ref(false)
   border-radius: 5%;
   background-color: #3a3845;
   color: #eeeeee;
+  top: 35px;
 }
 
 #container-dashboard {
